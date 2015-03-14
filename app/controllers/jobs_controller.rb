78fc2d62642cfb5a93fe80ad07e2d23df65f14c4
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-03-14 11:24:42 -0700

    initial commit

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,18 @@
+class JobsController < ApplicationController
+  def index
+    @jobs = Job.all
+  end
+  def new
+    @job = Job.new
+  end
+  def create
+    Job.create(job_params)
+    redirect_to jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+end
